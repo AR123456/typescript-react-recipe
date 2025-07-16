dc688798516817e5cfbf27e08d947782b4a7820b
AR123456 <coloradoz99@gmail.com>
2025-07-16 07:59:56 -0400

    re start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("api/recipe/search", async (req, res) => {
+app.get("/api/recipe/search", async (req, res) => {
   res.json({ message: "success" });
 });
 
