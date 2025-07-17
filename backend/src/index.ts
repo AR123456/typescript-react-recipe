0b17317b99348173c54ce99c7ac7b9eae1de10cd
AR123456 <coloradoz99@gmail.com>
2025-07-17 08:58:50 -0400

    adding some routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,16 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+// routes
 app.get("/api/recipe/search", async (req: Request, res: Response) => {
   res.json({ message: "success" });
 });
-
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {});
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
