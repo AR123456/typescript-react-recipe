71a119667cc576b04dc7defa1c9a282a6d57bbee
AR123456 <coloradoz99@gmail.com>
2025-07-18 09:25:23 -0400

    del fav

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ app.get("/api/recipe/search", async (req: Request, res: Response) => {
 app.get("/api/recipes/:recipeId/summary", async (req, res) => {});
 app.post("/api/recipes/favorite", async (req, res) => {});
 app.get("/api/recipes/favorite", async (req, res) => {});
+app.delete("/api/recipes/favorite", async (req, res) => {});
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
