f0a01cf36f184169dd7b615c9e39767578040b16
AR123456 <coloradoz99@gmail.com>
2025-07-21 06:59:46 -0400

    search recipes function

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -1,2 +1,9 @@
 // logic to search recipe api
 const apiKey = process.env.API_KEY;
+
+const searchRecipes = (searchTerm: string, page: number) => {
+  if (!apiKey) {
+    throw new Error("API Key not found ");
+  }
+  //    url of recipe endpoint
+};
