b3d88b921b8b0bbc3342c4cbda736b3cdced829c
AR123456 <coloradoz99@gmail.com>
2025-07-24 04:57:22 -0400

    starting on getting setter for search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-// dont really need to import React
-import React from "react";
-import "./App.css";
+// dont need to import React - but do its componetns
 
+import "./App.css";
+import { useState } from "react";
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("burgers");
   return <div>Hello from recipe app! </div>;
 };
 
