3bba5718c4f6e0db10ee2b6b49e42c022a702e0c
AR123456 <coloradoz99@gmail.com>
2025-07-25 06:52:31 -0400

    try catch to handle searchsubmit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,19 @@ import "./App.css";
 import { useState } from "react";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("burgers");
+  // api returns an array of results -
+  const [recipes, setRecipes] = useState([]);
+
+  // event handler for front end back end twice
+  const handleSearchSubmit = async () => {
+    try {
+      // passing in what back end expects a search term and page
+      const recipes = await api.searchRecipes(searchTerm, 1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>Hello from recipe app! </div>;
 };
 
