26b75f1b81b0622e11027c0d9bb3b10c634337d1
AR123456 <coloradoz99@gmail.com>
2025-07-25 06:53:17 -0400

    export api search function

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,14 @@
-const searchRecipes = async (searchTerm: string, page: number) => {
+export const searchRecipes = async (searchTerm: string, page: number) => {
   // the node back end  end point
   const baseUrl = new URL("http://localhost:5000/api/recipes/search");
   baseUrl.searchParams.append("searchTerm", searchTerm);
   //   page is a number but need to pass in as a string
   baseUrl.searchParams.append("page", String(page));
+  // fetch request
+  const response = await fetch(baseUrl);
+  if (!response.ok) {
+    throw new Error(`HTTP error Status: ${response.status}`);
+  }
+  //   send the json on
+  return response.json();
 };
