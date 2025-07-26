3c72209683db035d45de264e04a237807d860590
AR123456 <coloradoz99@gmail.com>
2025-07-26 10:03:49 -0400

    setting up map ts

--- frontend/src/types.ts
+++
@@ -1,9 +0,0 @@
-// interface is basicaly types but more versital
-
-export interface Recipe {
-  // id,title,image and type
-  id: number;
-  title: string;
-  image: string;
-  imageType: string;
-}
