2d845d81f196fe01b08fe5dda1ae216a592a605d
AR123456 <coloradoz99@gmail.com>
2025-07-29 07:36:44 -0400

    form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
 // dont need to import React - but do its componetns
 
 import "./App.css";
-import { useState, type FormEvent, useRef } from "react";
+import { useState, type FormEvent } from "react";
 import * as api from "./api";
 // get recipe from the ts types file
 import { Recipe } from "./types";
-import RecipeCard from "./components/RecipeCard";
 const App = () => {
-  // update hard coded search term with empty string - tell ts its a string
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState("burgers");
   // api returns an array of results -
   const [recipes, setRecipes] = useState<Recipe[]>([]);
-  // useRef hook for page number- page wont have to re render every time page increments
-  const pageNumber = useRef(1);
 
   // event handler for front end to back will be on submit button
   const handleSearchSubmit = async (event: FormEvent) => {
@@ -26,19 +22,7 @@ const App = () => {
       console.log(error);
     }
   };
-  const handleViewMoreClick = async () => {
-    // call recipes api pass in query page
-    // calculate what page number should be and pass along to the API
-    const nextPage = pageNumber.current + 1;
-    try {
-      // nextRecipes needs to be appended to the recipes array so the new result dosent blow away the page
-      const nextRecipes = await api.searchRecipes(searchTerm, nextPage);
-      // copy the recipes array add next page of recipes to it - save to state- trigger re render
-      setRecipes([...recipes, ...nextRecipes.results]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+
   return (
     <div>
       <form onSubmit={(event) => handleSearchSubmit(event)}>
@@ -54,12 +38,10 @@ const App = () => {
       {/* typescript needs to know why the type is of image and title, go to types.ts */}
       {recipes.map((recipe) => (
         <div>
-          <RecipeCard recipe={recipe} />
+          recipe image location:{recipe.image}
+          recipe title: {recipe.title}
         </div>
       ))}
-      <button className="view-more-button" onClick={handleViewMoreClick}>
-        View More
-      </button>
     </div>
   );
 };
