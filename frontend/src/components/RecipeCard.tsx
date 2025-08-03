abc6fbed0dc0045f87c35996abf481a35a292edf
AR123456 <coloradoz99@gmail.com>
2025-08-03 07:36:49 -0400

    trying some stuff out

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,14 +1,42 @@
-import type { Recipe } from "../types";
+// import type { Recipe } from "../types";
+
+// interface Props {
+//   recipe: Recipe;
+// }
+
+// const RecipeCard = ({ recipe }: Props) => {
+//   return (
+//     <div className="recipe-card">
+//       <img src={recipe.image} alt="recipe image" />
+//       <div className="recipe-card-title">
+//         <h3>{recipe.title}</h3>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RecipeCard;
+import { Recipe } from "../types";
+// import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 interface Props {
   recipe: Recipe;
+
+  onClick: () => void;
+  onFavouriteButtonClick: (recipe: Recipe) => void;
 }
 
-const RecipeCard = ({ recipe }: Props) => {
+const RecipeCard = ({ recipe, onClick }: Props) => {
   return (
-    <div className="recipe-card">
-      <img src={recipe.image} alt="recipe image" />
+    <div className="recipe-card" onClick={onClick}>
+      <img src={recipe.image}></img>
       <div className="recipe-card-title">
+        <span
+          onClick={(event) => {
+            event.stopPropagation();
+            onFavouriteButtonClick(recipe);
+          }}
+        ></span>
         <h3>{recipe.title}</h3>
       </div>
     </div>
--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -1,16 +1,64 @@
-import React from "react";
+// import React from "react";
 
-const RecipeModal = () => {
+// const RecipeModal = () => {
+//   return (
+//     <>
+//       <div className="overlay"></div>
+//       <div className="modal">
+//         <div className="modal-content">
+//           <div className="modal-header">
+//             <h2>Recipe Title</h2>
+//             <span className="close-btn">&times;</span>
+//           </div>
+//           <p>Recipe Summary</p>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default RecipeModal;
+import { useEffect, useState } from "react";
+import { RecipeSummary } from "../types";
+import * as RecipeAPI from "../api";
+
+interface Props {
+  recipeId: string;
+  onClose: () => void;
+}
+
+const RecipeModal = ({ recipeId, onClose }: Props) => {
+  const [recipeSummary, setRecipeSummary] = useState<RecipeSummary>();
+
+  useEffect(() => {
+    const fetchRecipeSummary = async () => {
+      try {
+        const summaryRecipe = await RecipeAPI.getRecipeSummary(recipeId);
+        setRecipeSummary(summaryRecipe);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchRecipeSummary();
+  }, [recipeId]);
+
+  if (!recipeSummary) {
+    return <></>;
+  }
+
   return (
     <>
       <div className="overlay"></div>
       <div className="modal">
         <div className="modal-content">
           <div className="modal-header">
-            <h2>Recipe Title</h2>
-            <span className="close-btn">&times;</span>
+            <h2>{recipeSummary.title}</h2>
+            <span className="close-btn" onClick={onClose}>
+              &times;
+            </span>
           </div>
-          <p>Recipe Summary</p>
+          <p dangerouslySetInnerHTML={{ __html: recipeSummary.summary }}></p>
         </div>
       </div>
     </>
