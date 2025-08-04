c52650c0193660f054a252418e724163e6d524c2
AR123456 <coloradoz99@gmail.com>
2025-08-04 06:24:11 -0400

    recipe props define on click

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -3,12 +3,12 @@ import type { Recipe } from "../types";
 interface Props {
   recipe: Recipe;
   // on click has to be a function that accepts a recipe and returns nothing
-  onClick: (recipe: Recipe) => void;
+  onClick: () => void;
 }
 
-const RecipeCard = ({ recipe }: Props) => {
+const RecipeCard = ({ recipe, onClick }: Props) => {
   return (
-    <div className="recipe-card">
+    <div className="recipe-card" onClick={onClick}>
       <img src={recipe.image} alt="recipe image" />
       <div className="recipe-card-title">
         <h3>{recipe.title}</h3>
