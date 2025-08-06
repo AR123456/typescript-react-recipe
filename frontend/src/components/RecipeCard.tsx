a28f406edc22e78591adf970b9dd8be3d5aaa877
AR123456 <coloradoz99@gmail.com>
2025-08-06 06:00:15 -0400

    going back

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,42 +1,14 @@
-// import type { Recipe } from "../types";
-
-// interface Props {
-//   recipe: Recipe;
-// }
-
-// const RecipeCard = ({ recipe }: Props) => {
-//   return (
-//     <div className="recipe-card">
-//       <img src={recipe.image} alt="recipe image" />
-//       <div className="recipe-card-title">
-//         <h3>{recipe.title}</h3>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RecipeCard;
-import { Recipe } from "../types";
-// import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import type { Recipe } from "../types";
 
 interface Props {
   recipe: Recipe;
-
-  onClick: () => void;
-  onFavouriteButtonClick: (recipe: Recipe) => void;
 }
 
-const RecipeCard = ({ recipe, onClick }: Props) => {
+const RecipeCard = ({ recipe }: Props) => {
   return (
-    <div className="recipe-card" onClick={onClick}>
-      <img src={recipe.image}></img>
+    <div className="recipe-card">
+      <img src={recipe.image} alt="recipe image" />
       <div className="recipe-card-title">
-        <span
-          onClick={(event) => {
-            event.stopPropagation();
-            onFavouriteButtonClick(recipe);
-          }}
-        ></span>
         <h3>{recipe.title}</h3>
       </div>
     </div>
