435ef744e2b59edbff4ea292ea2364e3b42150bb
AR123456 <coloradoz99@gmail.com>
2025-08-06 05:58:32 -0400

    modal html

--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const RecipeModal = () => {
-  return <div>RecipeModal</div>;
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className="modal">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h2>Recipe Title</h2>
+            <span className="close-btn">&times;</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RecipeModal;
