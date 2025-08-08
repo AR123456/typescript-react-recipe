3989c3b7ce3d3ff4eae0aebe6c1a372cedbc45fb
AR123456 <coloradoz99@gmail.com>
2025-08-08 06:57:05 -0400

    fixed recipe vs recipes

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 export const searchRecipes = async (searchTerm: string, page: number) => {
-  // the node back end  end point
-  const baseUrl = new URL("http://localhost:5000/api/recipe/search");
-  // console.log(baseUrl); http://localhost:5000/api/recipe/search?searchTerm=burger&page=1
+  // the node back end  end point -note this is recipes not recipe
+  const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+
   baseUrl.searchParams.append("searchTerm", searchTerm);
   //   page is a number but need to pass in as a string
   baseUrl.searchParams.append("page", String(page));
