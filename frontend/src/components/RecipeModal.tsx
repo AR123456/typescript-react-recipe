533d199c62177d2c85f02d21b20192d605b55bbc
AR123456 <coloradoz99@gmail.com>
2025-08-10 10:01:07 -0400

    getting some html back

--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
+import { RecipeSummary } from "../types";
 
 const RecipeModal = () => {
   // object of recipe data- account for in types.ts
-  const [recipeSummary, setRecipeSummary] = useState();
+  const [recipeSummary, setRecipeSummary] = useState<RecipeSummary>();
+  if (!recipeSummary) {
+    return <></>;
+  }
   return (
     <>
       <div className="overlay"></div>
       <div className="modal">
         <div className="modal-content">
           <div className="modal-header">
-            <h2>Recipe Title</h2>
+            <h2>{recipeSummary?.id}</h2>
             <span className="close-btn">&times;</span>
           </div>
-          <p>Recipe Summary</p>
+          {/* rendering HTML from api generally a no no  */}
+
+          <p
+            dangerouslySetInnerHTML={{
+              __html: recipeSummary.summary,
+            }}
+          ></p>
         </div>
       </div>
     </>
